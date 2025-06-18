65aeecc912e948102b97229e1688af2111552c24
Pinto Manuel <pintoydc@gmail.com>
2025-06-18 16:37:27 +0200

    Add comprehensive .gitignore for .NET projects

--- StockFlowPro.Infrastructure/obj/Debug/net8.0/StockFlowPro.Infrastructure.AssemblyInfo.cs
+++ StockFlowPro.Infrastructure/obj/Debug/net8.0/StockFlowPro.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockFlowPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fe219fb8e1b75bc53fe1e734086e0a98eede5b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6118829ab71f38c05393922633d66c14405d95e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockFlowPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockFlowPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
