05bcced478782cc0e7112ac043c47977746d3230
Pinto Manuel <pintoydc@gmail.com>
2025-06-22 08:09:20 +0200

    Product model created

---
+++ StockFlowPro.Web/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace StockFlowPro.Web.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal CostPerItem { get; set; }
+        public int NumberInStock { get; set; }
+    }
+}
