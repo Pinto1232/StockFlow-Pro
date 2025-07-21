861b5a03a440afe95c33ea1b384b5be65c1d986f
Pinto Manuel <pintoydc@gmail.com>
2025-07-22 00:03:48 +0200

    roles implementation is now working file

--- StockFlowPro.Infrastructure/obj/Debug/net8.0/StockFlowPro.Infrastructure.AssemblyInfo.cs
+++ StockFlowPro.Infrastructure/obj/Debug/net8.0/StockFlowPro.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockFlowPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e76d03286d7f04a953e8c8c93f561c20831b38c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2034aab4d4a58984e00e23a12f5d2e126fb5406")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockFlowPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockFlowPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
