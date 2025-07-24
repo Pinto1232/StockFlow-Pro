0851ba58e1382742ec5ca2fef5c8e24885dad993
Pinto Manuel <pintoydc@gmail.com>
2025-07-24 03:19:53 +0200

    updated changes

--- StockFlowPro.Domain.Tests/obj/Debug/net8.0/StockFlowPro.Domain.Tests.AssemblyInfo.cs
+++ StockFlowPro.Domain.Tests/obj/Debug/net8.0/StockFlowPro.Domain.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockFlowPro.Domain.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec6ceb91388dde76ba344ed122e8d652dc2c30d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a20831e98461aedaf113284c083ba8c5b8910a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockFlowPro.Domain.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockFlowPro.Domain.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StockFlowPro.Shared/obj/Debug/net8.0/StockFlowPro.Shared.AssemblyInfo.cs
+++ StockFlowPro.Shared/obj/Debug/net8.0/StockFlowPro.Shared.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Shared utilities, constants, extensions, and helpers for StockFlow Pro applicatio" +
     "ns")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec6ceb91388dde76ba344ed122e8d652dc2c30d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a20831e98461aedaf113284c083ba8c5b8910a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockFlowPro.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockFlow Pro Shared Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
